fe4d9b31b7b58a3043b378a779c870b1943895c7
ingleo <clgrhc@gmail.com>
2018-06-02 06:30:38 -0500

    added jwt validation for login

--- server/config/config.js
+++ server/config/config.js
@@ -4,6 +4,13 @@ process.env.PORT = process.env.PORT || 3000;
 //Entorno
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
+//Vencimiento de token
+// 60 seg * 60 min * 24 horas * 30 días
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+//Seed de autenticación
+process.env.SEED = process.env.SEED || 'seed-en-dev';
+
 //BD
 let urlDB;
 
